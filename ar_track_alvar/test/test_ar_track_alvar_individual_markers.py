93e880a4aa12d4ba7051f9ec8cadab22849f7b60
Patrick Musau <pmusau13ster@gmail.com>
2021-07-28 07:45:37 -0500

    Individual marker test

--- ar_track_alvar/test/test_ar_track_alvar_individual_markers.py
+++ ar_track_alvar/test/test_ar_track_alvar_individual_markers.py
@@ -14,9 +14,19 @@ import rclpy
 
 from stereo_msgs.msg import DisparityImage
 from sensor_msgs.msg import Image
+from ar_track_alvar_msgs.msg import AlvarMarkers
 
-
+# Test Parameters
 bag_name = os.path.join(get_package_share_directory('ar_track_alvar'),'ar_track_alvar_4markers_tork_2017-02-08-11-21-14.bag')
+cam_image_topic ="/camera/image_raw" 
+cam_info_topic = "/camera/camera_info" 	
+marker_margin="2"
+marker_resolution="5"
+marker_size="2.3"
+max_new_marker_error="0.08"
+max_frequency="100"
+max_track_error="0.2"
+output_frame="/camera"
 
 @pytest.mark.rostest
 def generate_test_description():
@@ -24,19 +34,22 @@ def generate_test_description():
     return LaunchDescription([
         # DisparityNode
 
-
         launch.actions.ExecuteProcess(
             cmd=['ros2', 'bag', 'play', '-s',"rosbag_v2",bag_name],
             output='screen'
         ),
 
-        # Node(
-        #     package='ros2bag',
-        #     executable='bag',
-        #     name='bag_node',
-        #     arguments=["play","-s","rosbag_v2",bag_name],
-        #     output='screen'
-        # ),
+        Node(
+            package='ar_track_alvar',
+            executable='individualMarkers',
+            name='invidual_markers',
+            remappings=[
+                ("camera_image", cam_image_topic),
+                ("camera_info",cam_info_topic)
+            ],
+            arguments=[marker_size, max_new_marker_error, max_track_error, cam_image_topic, cam_info_topic, output_frame],
+            output='screen'
+        ),
         launch_testing.actions.ReadyToTest(),
     ])
 
@@ -56,8 +69,8 @@ class TestArTrack(unittest.TestCase):
         # Expect the disparity node to publish on '/disparity' topic
         msgs_received = []
         self.node.create_subscription(
-            Image,
-            '/camera/image_raw',
+            AlvarMarkers,
+            "ar_pose_marker",
             lambda msg: msgs_received.append(msg),
             1
         )
